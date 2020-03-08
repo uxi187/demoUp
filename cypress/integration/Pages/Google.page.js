034d310b625e577ac6314d8003ac3e6d6ebabe5c
Milan Suka <sukamilan@gmail.com>
2020-03-08 16:24:12 +0100

    more checks added

--- cypress/integration/Pages/Google.page.js
+++ cypress/integration/Pages/Google.page.js
@@ -8,6 +8,23 @@ export const Google = {
         searchByVoice: 'div[aria-label="Search by voice"]',
         searchBtn: 'button[aria-label="Google Search"]',
         searchSuggestion: '.UUbT9 ul[role="listbox"]',
-        searchSuggestionList: 'li[role="presentation"]'
+        searchSuggestionList: 'li[role="presentation"]',
+        imageOption: {
+            title: '#qbhwr .qbtt',
+            subTitle: '#qbf .qbwr',
+            close: '#qbhwr #qbx',
+            inputPasteURL: '#qbui',
+            dialog: '#qbp'
+
+        },
+        footer: {
+            left: '#footcnt #fsl',
+            right: '#footcnt #fsr',
+            link: '.Fx4vi'
+        },
+        googleApps:{
+            apps: '#gbw a[title="Google apps"]',
+            account: '#gbw .gb_ce.gb_4.gb_Wc'
+        }
     }
 }
--- cypress/integration/Tests/google.spec.js
+++ cypress/integration/Tests/google.spec.js
@@ -8,7 +8,7 @@ describe("Google Image tests", () => {
     })
 
 
-    it("Check Google Imagine page elements", () => {
+    it("Check Google Imagine action elements", () => {
 
         //check site title
         cy.title().should('eq', 'Google Images')
@@ -43,25 +43,74 @@ describe("Google Image tests", () => {
         //check suggestion
         cy.get(google.selectors.searchSuggestion)
             .should('be.visible')
-          .get(google.selectors.searchSuggestionList)
+            .get(google.selectors.searchSuggestionList, { timeout: 3000 })
             .contains(SEARCH_CRITERIA.toLocaleLowerCase())
     })
 
-    it("Search for image", () => {
-        const SEARCH_CRITERIA = "Super Mario"
+    it("Check search by image options", () => {
 
-        //check is search criteria typed in search field
-        cy.get(google.selectors.search).type(SEARCH_CRITERIA)
-            .should('have.value', SEARCH_CRITERIA)
+        const INPUT_URL = 'https://google.com'
 
-        cy.get(google.selectors.searchBtn)
+        cy.get(google.selectors.searchByImage).click()
+
+        //check pop up title
+        cy.get(google.selectors.imageOption.title)
+            .should('have.text', 'Search by image')
+
+        //check pop up subtitle
+        cy.get(google.selectors.imageOption.subTitle)
+            .should('contain.text', 'Paste image URL')
+            .and('contain.text', 'Upload an image')
+
+        //check paste url option
+        cy.get(google.selectors.imageOption.inputPasteURL)
+            .should('be.visible')
+            .type(INPUT_URL)
+            .should('have.value', INPUT_URL)
+
+        //check close button is visible an click on it
+        cy.get(google.selectors.imageOption.close)
             .should('be.visible')
             .click()
 
-        //redirections are not supported in Cypress
-        // cy.get('div[data-id="GRID_STATE0"]')
-
+        //check dialog is not visible any more
+        cy.get(google.selectors.imageOption.dialog)
+            .should('not.be.visible')
     })
 
+    it("Check page footer", () => {
 
+        //check left footer
+        cy.get(google.selectors.footer.left)
+            .should('contain.text', 'Advertising')
+            .and('contain.text', 'Business')
+            .and('contain.text', 'About')
+            .and('contain.text', 'How Search works')
+
+        //check right footer
+        cy.get(google.selectors.footer.right)
+            .should('contain.text', 'Privacy')
+            .and('contain.text', 'Terms')
+            .and('contain.text', 'Settings')
+
+        //check for footer link
+        cy.get(google.selectors.footer.link).each($ft => {
+            expect($ft).to.have.attr('href')
+        })
+    })
+
+    it("Check google apps", () => {
+
+        //check google apps
+        cy.get(google.selectors.googleApps.apps)
+            .should('have.attr', 'href')
+            .and("include", "/about/products?");
+
+        //check google account
+        cy.get(google.selectors.googleApps.account)
+            .should('have.text', 'Sign in')
+            .and('have.attr', 'href')
+            .and("include", "https://accounts.google.com/ServiceLogin")
+
+    })
 })
